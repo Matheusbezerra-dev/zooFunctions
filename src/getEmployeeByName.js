3ca7966bbc7ab63e5295d3ec2134f4ab953c8aca
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-29 17:35:28 -0400

    finalizando o exercicio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName !== 'string') {
+    return {};
+  }
+  const findName = data.employees.find((info) =>
+    employeeName === info.firstName || employeeName === info.lastName);
+  return findName;
 }
 
 module.exports = getEmployeeByName;
