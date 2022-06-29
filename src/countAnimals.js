1bf2cc959aa4be79531d44465d74aa8d150b1dfd
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-29 19:59:07 -0400

    finalizando o exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const c = {};
+  if (!animal) {
+    data.species.forEach((element) => {
+      c[element.name] = element.residents.length;
+    });
+    return c;
+  }
+  if (!animal.sex) {
+    return data.species.find((element) => element.name === animal.specie).residents.length;
+  }
+  return data.species.find((element) => element.name === animal.specie).residents
+    .filter((element) => element.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
