32e37dec328ec6510de7ed620f2b81f6b585584c
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-30 17:34:33 -0400

    finalizando 12 13 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,15 +1,5 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-const hours = {
-  Tuesday: { open: 8, close: 6 },
-  Wednesday: { open: 8, close: 6 },
-  Thursday: { open: 10, close: 8 },
-  Friday: { open: 10, close: 8 },
-  Saturday: { open: 8, close: 10 },
-  Sunday: { open: 8, close: 8 },
-  Monday: { open: 0, close: 0 },
-};
-
 describe('Testes da função getOpeningHours', () => {
   it('Teste não passando argumentos. Deverá retornar o objeto', () => {
     const actual = getOpeningHours();
