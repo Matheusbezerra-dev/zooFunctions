9e27d3e074b6878269a7c22dc5bb3c1da1c4fadf
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-30 20:00:45 -0400

    finalizando 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const numberChilds = entrants.filter(({ age }) => age < 18).length;
+  const nummberAdults = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const numberSenior = entrants.filter(({ age }) => age >= 50).length;
+  const numberPeople = { child: numberChilds, adult: nummberAdults, senior: numberSenior };
+  return numberPeople;
+};
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  if (!Array.isArray(entrants)) return 0;
+  const people = countEntrants(entrants);
+  const sum = (people.child * 20.99) + (people.adult * 49.99) + (people.senior * 24.99);
+  return sum;
+};
 
 module.exports = { calculateEntry, countEntrants };
