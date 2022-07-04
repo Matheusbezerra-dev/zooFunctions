400e3f41c8ee9da6fc5a513d8b658f33162b9a45
Matheus Bezerra <mabezerra2@gmail.com>
2022-07-04 14:11:41 -0400

    finalizando 9 e 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const getSpecie = (paramTwon) => {
+  const info = Object.values(paramTwon)[0];
+  const employee = data.employees.find(({ id: one, firstName: two, lastName: three }) => [
+    one, two, three].includes(info));
+  if (!employee) throw new Error('Informações inválidas');
+  const { id, firstName, lastName, responsibleFor: animalIds } = employee;
+  const specie = data.species.filter(({ id: animalId }) => animalIds.some(
+    (animal) => animal === animalId));
+
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: specie.map(({ name }) => name),
+    locations: specie.map(({ location }) => location) 
+  };
+};
+
+const getEmployeesCoverage = (param) => {
+  if (param) return getSpecie(param);
+  return data.employees.reduce((acc, { id }) => [...acc, getSpecie({ id })], []);
+};
 
 module.exports = getEmployeesCoverage;
