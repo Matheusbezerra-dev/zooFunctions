a5a9bb271c374578bccd19e4e0ab17e473e99f79
Matheus Bezerra <mabezerra2@gmail.com>
2022-07-04 15:03:16 -0400

    finalizando 15

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,28 +1,35 @@
 const data = require('../data/zoo_data');
 
-const sorting = (number) => data.species.reduce((acc, specie) => ({ ...acc,
+const sorting = (number) => data.species.reduce((acc, specie) => ({
+  ...acc,
   [specie.location]: data.species.map((num) => {
     if (num.location !== specie.location) return null;
     if (number.sex) {
-      return { [num.name]: num.residents.filter((element) => element.sex === number.sex)
-        .map((numb) => numb.name).sort() };
+      return {
+        [num.name]: num.residents.filter((element) => element.sex === number.sex)
+          .map((numb) => numb.name).sort(),
+      };
     }
     return { [num.name]: num.residents.map((numberTwo) => numberTwo.name).sort() };
   }).filter((elements) => elements !== null),
 }), {});
 
-const nameSearch = (arg) => data.species.reduce((acc, specie) => ({ ...acc,
+const nameSearch = (arg) => data.species.reduce((acc, specie) => ({
+  ...acc,
   [specie.location]: data.species.map((numb) => {
     if (numb.location !== specie.location) return null;
     if (arg.sex) {
-      return { [numb.name]: numb.residents.filter((element) => element.sex === arg.sex)
-        .map((numberFour) => numberFour.name) };
+      return {
+        [numb.name]: numb.residents.filter((element) => element.sex === arg.sex)
+          .map((numberFour) => numberFour.name),
+      };
     }
     return { [numb.name]: numb.residents.map((numberTwo) => numberTwo.name) };
   }).filter((element) => element !== null),
 }), {});
 
-const namesByLocation = data.species.reduce((acc, specie) => ({ ...acc,
+const namesByLocation = data.species.reduce((acc, specie) => ({
+  ...acc,
   [specie.location]: data.species.map((el) => {
     if (el.location === specie.location) {
       return el.name;
